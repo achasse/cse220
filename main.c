fafc47f9c09787d5a7525798675c656b8e9736d0
achasse <achasse@asu.edu>
2014-02-05 14:39:03 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "common.h"
+
+int main (int argc, const char *argv[])
+{
+    FILE *source_file;
+    char source_name[MAX_FILE_NAME_LENGTH];
+    char date[DATE_STRING_LENGTH];
+    bool loop_check = TRUE;
+
+    while(loop_check)
+    {
+		loop_check = get_source_line(*src_file, source_name[MAX_FILE_NAME_LENGTH], date[DATE_STRING_LENGTH]);
+	}
+   /* Missing Code Here */
+    return 0;
+}
+FILE *init_lister(const char *name, char source_file_name[], char dte[])
+{
+    time_t timer;
+    FILE *file;
+
+    /* Missing Code Here */
+    return file;
+}
+BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
+{
+    char print_buffer[MAX_SOURCE_LINE_LENGTH + 9];
+    char source_buffer[MAX_SOURCE_LINE_LENGTH];
+    static int line_number = 0;											//initialize static variable to keep track of line number, set to 0 for inception
+
+
+
+    if (fgets(source_buffer, MAX_SOURCE_LINE_LENGTH, src_file ) != NULL) //This is missing a condition
+    {
+		sprintf(print_buffer, line_number + " " + source_buffer, ... );	//sends line_number and source_buffer to print_buffer
+		print_line(print_buffer, src_name, todays_date);				////call print_line method and pass in print_buffer, src_name, and the date
+		line_number++;													//increment line_number for next function call
+		/*  Missing Code Here */
+        return (TRUE);
+    }
+    else
+    {
+        return (FALSE);
+    }
+}
+
---
+++ print.c
@@ -0,0 +1,37 @@
+#include "common.h"
+
+static void print_page_header(char source_name[], char date[]);
+
+void print_line(char line[], char source_name_to_print[], char date_to_print[])
+{
+    char save_ch;
+    char *save_chp = NULL;
+    static int line_count = MAX_LINES_PER_PAGE;
+
+    if (++line_count > MAX_LINES_PER_PAGE)
+    {
+        print_page_header(source_name_to_print, date_to_print);
+    }
+    if (strlen(line) > MAX_PRINT_LINE_LENGTH)
+    {
+		save_chp = TRUE;
+        // truncate line
+    }
+    if (save_chp)
+    {
+		save_ch = line;
+        /* Missing Code Here */
+    }
+    /* Missing Code Here */
+    if (save_chp)
+    {
+        /* Missing Code Here */
+    }
+}
+static void print_page_header(char source_name[], char date[])
+{
+    static int page_number = 0;
+	puts(source_name + " " + date + " " + page_number);
+	page_number++;
+    /* Missing Code Here */
+}
