a69630563d46d4bfddf799e19d9b7ff84aa079dd
Jake Borg <ebmixed@gmail.com>
2014-02-06 14:38:09 -0700

    2014 2 (February) 6 Meeting
    Added pseuedocode, print format in print_page_header

--- print.c
+++ print.c
@@ -31,7 +31,7 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
 static void print_page_header(char source_name[], char date[])
 {
     static int page_number = 0;
-	puts(source_name + " " + date + " " + page_number);
+	puts(source_name + " " + date + " " + page_number); // printf("%s %s %d\n", source_name, date, page_number);
 	page_number++;
     /* Missing Code Here */
 }
