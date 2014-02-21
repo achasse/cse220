c1e4524d1f3145320d68a2f001725c352302aaa8
achasse <achasse@asu.edu>
2014-02-21 10:41:54 -0700

    line_count switch

--- print.c
+++ print.c
@@ -11,6 +11,7 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     if (++line_count > MAX_LINES_PER_PAGE)
     {
         print_page_header(source_name_to_print, date_to_print);
+	line_count = 0;
     }
     // Prints a substring of line[] using the max length as the size. Doesn't modify line[], no need to restore to original size
     printf("%4d: %.*s", line_count, MAX_PRINT_LINE_LENGTH, line);
@@ -20,7 +21,6 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
 static void print_page_header(char source_name[], char date[])
 {
 	static int page_number = 0;
-	line_count = 0;
 	printf("Page %4d %*s\t %30s\t \n", page_number, MAX_PRINT_LINE_LENGTH/2, source_name, date);
 	page_number++;
 }
