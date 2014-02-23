9a40ed82e0aab3cc5812a6c4fc5ef641d08a8393
dougdliu <dougdliu@gmail.com>
2014-02-22 23:35:25 -0700

    Update testPrintLine.c

--- testPrintLine.c
+++ testPrintLine.c
@@ -6,24 +6,37 @@
 
 #include "common.h"
 
-int main (int argc, const char *argv[])
+int main ()
 {
-    FILE *source_file;
-    char source_name[MAX_FILE_NAME_LENGTH];
-    char date[DATE_STRING_LENGTH];
-    char loop_check = TRUE;
+    char source_name1[MAX_FILE_NAME_LENGTH] = "/User/dliu30/Downloads/Test.c";
+    char source_name2[MAX_FILE_NAME_LENGTH] = "/User/dliu30/Downloads/Test";
+    char date1[DATE_STRING_LENGTH] = "Tue Feb 18 20:56:59 2014";
+    char date2[DATE_STRING_LENGTH] = "Feb Tue 18 20:56:59 2014";
 
-    //copy name of file from input argument into source_name    
-    strcpy(source_name, argv[1]);
-    //FILE source_file pointer will point to return from init_lister: a readable file
-    print_line(source_name, source_name, date)//Tests the parameter source_name, source_name, date
-    print_line(source_name, source_name, dat)//Tests the parameter source_name, source_name, dat
-    print_line(source_name, source_nam, date)//Tests the parameter source_name, source_nam, date
-    print_line(source_name, source_nam, dat)//Tests the parameter source_name, source_nam, dat
-    print_line(source_nam, source_name, date)//Tests the parameter source_nam, source_name, date
-    print_line(source_nam, source_name, dat)//Tests the parameter source_nam, source_name, dat
-    print_line(source_nam, source_nam, date)//Tests the parameter source_nam, source_nam, date
-    print_line(source_nam, source_nam, dat)//Tests the parameter source_nam, source_nam, dat
+    print_line(source_name1, source_name1, date1)//Valid as all parameters are valid.
+    print_line(source_name1, source_name1, date2)//Invalid as date2 has wrong format
+    print_line(source_name1, source_name2, date1)//Invalid as source_name2 is not a valid file
+    print_line(source_name1, source_name2, date2)//Invalid as source_name2 and date2 is wrong
+    print_line(source_name2, source_name1, date1)//Invalid as source_name2 is wrong.
+    print_line(source_name2, source_name1, date2)//Invalid as source_name2 and date2 is wrong.
+    print_line(source_name2, source_name2, date1)//Invalid as source_name2 is wrong.
+    print_line(source_name2, source_name2, date2)//Invalid as source_name2 and date2 is wrong.
  
     return 0;
 }//end main function
+
+void print_line(char line[], char source_name_to_print[], char date_to_print[])
+{
+    // Initialize line_count to the max lines per page so header is printed before first line
+    //of first page.
+    static int line_count = MAX_LINES_PER_PAGE;
+
+    //  Test if we need to make a new page - if the line count exceeded the maximum
+    if (++line_count > MAX_LINES_PER_PAGE)
+    {
+        print_page_header(source_name_to_print, date_to_print);
+	line_count = 0;
+    }
+    // Prints a substring of line[] using the max length as the size. Doesn't modify line[], no need to restore to original size
+    printf("%.*s", MAX_PRINT_LINE_LENGTH, line);
+}
